b86d0dc582bb38a266743728a444a61df677c6d7
Mateus <mtauws@gmail.com>
2022-01-26 20:27:35 -0300

    nivo is show up

--- src/components/GraphTeam/index.jsx
+++ src/components/GraphTeam/index.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-bind */
 import React, { useEffect, useState } from "react";
-import { ResponsiveBar } from "@nivo/bar";
+import { Bar } from "@nivo/bar";
 import fire from "../../fire.jsx";
 
 function GraphTeam() {
@@ -16,34 +18,74 @@ function GraphTeam() {
         });
     };
     getTodos();
-  }, []);
-  const arrayOfObj = Object.entries(todos).map((e) => ({
-    [e[0]]: e[1],
-  }));
 
-  console.log(arrayOfObj);
+    // const form = Object.values(todos[0]);
+  }, []);
+  // const arrayOfObj = Object.entries(todos).map((e) => ({
+  //   [e[0]]: e[1],
+  // }));
+  const d = Object.keys(todos).map((key) => {
+    const v = todos[key].value;
+    if (v >= 10 && v <= 20) todos[key].value = 7;
+    else if (v > 20 && v <= 40) todos[key].value = 8;
+    return todos[key];
+  });
+  const data = [
+    {
+      satisfaction: "10",
+      team: 15,
+    },
+    {
+      satisfaction: 10,
+      team: "15 ou mais",
+    },
+    {
+      satisfaction: 6,
+      team: "15 ou mais",
+    },
+    {
+      satisfaction: 4,
+      team: "1 até 3",
+    },
+    {
+      satisfaction: 6,
+      team: "4 até 6",
+    },
+  ];
+  console.log(d);
   return (
-    <ResponsiveBar
-      // data={[todos]}
-      keys={["degress"]}
-      indexBy="day"
-      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-      padding={0.4}
-      valueScale={{ type: "linear" }}
-      colors="#3182CE"
-      animate
-      enableLabel={false}
-      axisTop={null}
-      axisRight={null}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "degrees",
-        legendPosition: "middle",
-        legendOffset: -40,
-      }}
-    />
+    // style={{ height: 200 }}
+    <div>
+      <Bar
+        width={600}
+        height={400}
+        margin={{ top: 60, right: 80, bottom: 60, left: 80 }}
+        data={data}
+        indexBy="country"
+        keys={["hot dogs"]}
+        labelTextColor="inherit:darker(1.4)"
+        enableGridX
+        theme={{
+          axis: {
+            ticks: {
+              line: {
+                stroke: "green",
+              },
+              text: {
+                fill: "red",
+              },
+            },
+          },
+          grid: {
+            line: {
+              stroke: "pink",
+              strokeWidth: 2,
+              strokeDasharray: "4 4",
+            },
+          },
+        }}
+      />
+    </div>
   );
 }
 
