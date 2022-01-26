cf19663d83282e92025b416cd155bf6bfb1ebc9d
Mateus <mtauws@gmail.com>
2022-01-26 18:06:52 -0300

    convert obj to array

--- src/components/GraphTeam/index.jsx
+++ src/components/GraphTeam/index.jsx
@@ -15,41 +15,13 @@ function GraphTeam() {
           setTodos(snapshot.val());
         });
     };
-    console.log(todos);
     getTodos();
   }, []);
-  // const result = Object.keys(todos).map((key) => [Number(key), todos[key]]);
-  // console.log(result);
-  const data = [
-    {
-      day: "Monday",
-      degress: 59,
-    },
-    {
-      day: "Tuesday",
-      degress: 61,
-    },
-    {
-      day: "Wednesday",
-      degress: 55,
-    },
-    {
-      day: "Thursday",
-      degress: 78,
-    },
-    {
-      day: "Friday",
-      degress: 71,
-    },
-    {
-      day: "Saturday",
-      degress: 56,
-    },
-    {
-      day: "Sunday",
-      degress: 67,
-    },
-  ];
+  const arrayOfObj = Object.entries(todos).map((e) => ({
+    [e[0]]: e[1],
+  }));
+
+  console.log(arrayOfObj);
   return (
     <ResponsiveBar
       // data={[todos]}
