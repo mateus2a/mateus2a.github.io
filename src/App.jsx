785b5441f845bda6112b5e21e1d627006e03c8d6
Mateus <mtauws@gmail.com>
2022-01-28 17:57:19 -0300

    feat: insert box of the page into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
 import React, { Component } from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 import Form from "./components/Form/index.jsx";
 import GraphTeam from "./components/GraphTeam/index.jsx";
 import Welcome from "./components/Welcome/index.jsx";
+
 import "./App.css";
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -11,9 +15,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Welcome />
-        <Form />
-        <GraphTeam />
+        <div className="cointainer">
+          <CssBaseline />
+          <Container fixed>
+            <Box
+              sx={{
+                bgcolor: "#F5F6FF",
+                height: "60vh",
+                borderRadius: 6,
+                boxShadow: 1,
+              }}
+              className="box"
+            >
+              <Welcome />
+              <Form />
+            </Box>
+          </Container>
+        </div>
       </div>
     );
   }
--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,36 +1,22 @@
 import React from "react";
 import $ from "jquery";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
 import WelcolmeCat from "../../assets/welcome_cats_thqn.svg";
 import Button from "../Button/index.jsx";
+
 import "./style.css";
 
 function initForm() {
   $(".welcome").css("display", "none");
+  $(".form").css("display", "block");
 }
 
 function Welcome() {
   return (
     <div className="center">
-      <CssBaseline />
-      <Container fixed>
-        <Box
-          sx={{
-            bgcolor: "#F5F6FF",
-            height: "60vh",
-            borderRadius: 6,
-            boxShadow: 1,
-          }}
-          className="box"
-        >
-          <div className="welcome">
-            <img src={WelcolmeCat} alt="Welcome Cat" className="welcome-cat" />
-            <Button title="Preencher Form" action={initForm} />
-          </div>
-        </Box>
-      </Container>
+      <div className="welcome">
+        <img src={WelcolmeCat} alt="Welcome Cat" className="welcome-cat" />
+        <Button title="Preencher Formulário" action={initForm} />
+      </div>
     </div>
   );
 }
