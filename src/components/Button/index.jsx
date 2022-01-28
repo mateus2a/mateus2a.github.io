6b904bae5d3ccbf238470bcf51ab8a66cd061a2c
Mateus <mtauws@gmail.com>
2022-01-28 16:05:16 -0300

    welcome-page has been styled

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.css";
+
+function Button() {
+  return <div className="button" />;
+}
+
+export default Button;
--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import WelcolmeCat from "../../assets/welcome_cats_thqn.svg";
+
+import "./style.css";
 
 function Welcome() {
   return (
-    <div>
-      <p>Welcome</p>
+    <div className="center">
+      <CssBaseline />
+      <Container fixed>
+        <Box
+          sx={{
+            bgcolor: "#F5F6FF",
+            height: "70vh",
+            borderRadius: 6,
+            boxShadow: 1,
+          }}
+        >
+          <div className="welcome">
+            <img src={WelcolmeCat} alt="Welcome Cat" className="welcome-cat" />
+          </div>
+        </Box>
+      </Container>
     </div>
   );
 }
